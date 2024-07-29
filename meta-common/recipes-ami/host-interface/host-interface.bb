9994751c5994b33de3cbb2cd1745594b7f7a666e
seancheng1 <seancheng@ami.com>
2024-07-29 09:47:59 +0800

    Fix Lan over USB issue.

--- meta-common/recipes-ami/host-interface/host-interface.bb
+++ meta-common/recipes-ami/host-interface/host-interface.bb
@@ -2,21 +2,23 @@ SUMMARY = "Host interface intialization"
 SECTION = "application"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+DEPENDS += "systemd"
 
 SRC_URI = " \
-    file://configure_usb_gadget.sh \
+    file://create_usbeth.sh \
     file://host-interface.service \
     "
 
 S = "${WORKDIR}"
 
-inherit systemd
+inherit allarch systemd
 
 SYSTEMD_SERVICE:${PN} += "host-interface.service"
 
 do_install() {
     install -d ${D}${bindir}
-    install -m 0755 ${WORKDIR}/configure_usb_gadget.sh  ${D}${bindir}
+    install -m 0755 ${WORKDIR}/create_usbeth.sh  ${D}${bindir}
     install -d ${D}${base_libdir}/systemd/system
     install -m 0644 ${S}/host-interface.service ${D}${base_libdir}/systemd/system
+
 }
---
+++ meta-common/recipes-ami/host-interface/host-interface/create_usbeth.sh
@@ -0,0 +1,95 @@
+#! /bin/sh
+
+eth_conf_directory="/sys/kernel/config/usb_gadget/eth"
+dev_name="1e6a0000.usb-vhub"
+
+create_eth() {
+    # create gadget
+    mkdir "${eth_conf_directory}"
+    cd "${eth_conf_directory}" || exit 1
+
+    # Set USB gadget Vendor ID and Product ID
+    echo 0x046b > idVendor
+    echo 0xFFb0 > idProduct
+    
+    # Set USB gadget Class, SubClass, and Protocol
+    echo 0x02 > bDeviceClass
+    echo 0x00 > bDeviceSubClass
+    echo 0x00 > bDeviceProtocol
+    
+    # Set device version
+    echo 0x0100 > bcdDevice
+    
+    # Create strings directory and set serial number, manufacturer, and product name
+    mkdir strings/0x409
+    echo "American Megatrends Inc" > strings/0x409/manufacturer
+    echo "Virtual Ethernet." > strings/0x409/product
+    echo "1234567890" > strings/0x409/serialnumber
+
+    # Create interface for ECM and configure it.
+    mkdir functions/ecm.usb0
+    mkdir configs/c.2
+    echo 0 > configs/c.2/MaxPower
+    echo 0xC0 > configs/c.2/bmAttributes
+    mkdir configs/c.2/strings/0x409
+
+    # Rename 
+    echo hostusb%d > functions/ecm.usb0/ifname
+
+    # Create configuration for rndis
+    mkdir functions/rndis.usb0
+    echo RNDIS > functions/rndis.usb0/os_desc/interface.rndis/compatible_id
+    echo 5162001 > functions/rndis.usb0/os_desc/interface.rndis/sub_compatible_id
+    mkdir configs/c.1
+    echo 0 > configs/c.1/MaxPower
+    echo 0xC0 > configs/c.1/bmAttributes
+    mkdir configs/c.1/strings/0x409
+    
+    
+
+
+    # Link ECM and RNDIS functions to their respective configurations
+    ln -s functions/ecm.usb0 configs/c.2/
+    ln -s functions/rndis.usb0 configs/c.1/
+    ln -s configs/c.1 os_desc/c.1
+}
+
+connect_eth() {
+    if ! grep -q "${dev_name}:p" UDC; then
+        i=0
+        num_ports=5
+        base_usb_dir="/sys/bus/platform/devices/${dev_name}/${dev_name}:p"
+        while [ "${i}" -lt "${num_ports}" ]; do
+            port=$(("${i}" + 1))
+            i="${port}"
+            if [ ! -e "${base_usb_dir}${port}/gadget/suspended" ]; then
+                break
+            fi
+        done
+        echo "${dev_name}:p${port}" > UDC
+    fi
+}
+
+
+if [ ! -e "${eth_conf_directory}" ]; then
+    create_eth
+else
+    cd "${eth_conf_directory}" || exit 1
+fi
+
+if [ "$1" = "connect" ]; then
+    connect_eth
+    ## Assigning MAC address
+    USB0_MAC=`dmesg | grep "hostusb0: MAC" | cut -d ' ' -f 7`
+    ifconfig hostusb0 down
+    ifconfig hostusb0 hw ether $USB0_MAC
+    ifconfig hostusb0 169.254.0.17 netmask 255.255.0.0
+    ifconfig hostusb0 up
+
+else
+    echo >&2 "Invalid option: $1. Use 'connect'."
+    exit 1
+fi
+
+
+
